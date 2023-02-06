2c42b4ccad7107353fd55d655ee47d1b93867fec
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-02-06 22:08:02 +0100

    fix linter error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import './style.css';
 import displayData from './modules/displayData.js';
-import picture from './images/picture.jpg';
 
 window.onload = () => {
   displayData();
 };
-
-const image = document.getElementById('picture');
-image.src = picture;
