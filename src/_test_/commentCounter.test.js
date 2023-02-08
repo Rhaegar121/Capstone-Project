fb0f8c762cff9d460b4089b7d1beb92699852cdb
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-08 20:08:20 +0630

    fix eslint errors

--- src/_test_/commentCounter.test.js
+++ src/_test_/commentCounter.test.js
@@ -1,23 +1,23 @@
 /** @jest-environment jsdom */
-import countComments from "../modules/commentCounter";
+import countComments from '../modules/commentCounter.js';
 
 const movies = [
-    {
-      id: 1,
-      title: 'Arrow',
-    },
-    {
-      id: 2,
-      title: 'Flash',
-    },
-    {
-      id: 3,
-      title: 'Supernatural',
-    },
-  ];
+  {
+    id: 1,
+    title: 'Arrow',
+  },
+  {
+    id: 2,
+    title: 'Flash',
+  },
+  {
+    id: 3,
+    title: 'Supernatural',
+  },
+];
 
- describe('count comment of a particular movie', () => {
-    test('should return an array with 8 objects', () => { 
-        expect(countComments(movies)).toEqual(3)
-     })
- })
+describe('count comment of a particular movie', () => {
+  test('should return an array with 8 objects', () => {
+    expect(countComments(movies)).toEqual(3);
+  });
+});
