4f19843f9d43435716e9638f2222fedb26ec1793
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-02-08 11:33:10 +0100

    Finish setup for comment section

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -100,7 +100,6 @@ const displayData = async () => {
         updateComment(movie.id);
         
       })
-      updateComment(movie.id);
 
       const closeMovie = document.querySelector(`#closeBtn${index}`);
       closeMovie.addEventListener('click', () => comment.classList.add('hide'));
