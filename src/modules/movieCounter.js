7d1aebf22cda19fa16062fa73d603c69898828fd
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-09 12:44:11 +0630

    change movieCount function

--- src/modules/movieCounter.js
+++ src/modules/movieCounter.js
@@ -1,3 +1,8 @@
-const movieCount = (movie) => movie.length;
+const movieCount = (movie) => {
+  let count = 0;
+  for (let i = 0; i < movie.length; i += 1) {
+    count += 1;
+  } return count;
+};
 
 export default movieCount;
